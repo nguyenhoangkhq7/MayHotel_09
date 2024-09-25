818bee393d1f2865a23ce24db9ca30e36b5362a9
nguyen hoang khang (nhk) <nguyenhoangkhq7@gmail.com>
2024-09-26 01:17:22 +0700

    init bonus

---
+++ src/main/java/icon/icon.java
@@ -0,0 +1,14 @@
+/*
+    *MayHotel  day creative: 9/26/2024
+    version: 2023.2  IntelliJ IDEA
+    author: Nguyễn Hoàng Khang  */
+    /*
+       *class description:
+            write description right here   
+     */
+
+
+package icon;
+
+public class icon {
+}
