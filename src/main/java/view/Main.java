c7cdd55bd544e12a370c9a650a4c2c52116205ae
nguyen hoang khang (nhk) <nguyenhoangkhq7@gmail.com>
2024-09-26 01:14:48 +0700

    initial

---
+++ src/main/java/view/Main.java
@@ -0,0 +1,17 @@
+/*
+    *MayHotel  day creative: 9/26/2024
+    version: 2023.2  IntelliJ IDEA
+    author: Nguyễn Hoàng Khang  */
+    /*
+       *class description:
+            write description right here   
+     */
+
+
+package view;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
